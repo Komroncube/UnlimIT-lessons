a79116f9e4101c4e6ce8787000f88dfc307c467f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 11:58:00 +0500

    Add exponentialBooster pipe to boost number values

--- src/app/pipes/exponential-booster.pipe.ts
+++ src/app/pipes/exponential-booster.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * @description This pipe is used to boost the value of a number by an exponent
+ * @example <div>{{ 2 | exponentialBooster: 3 }}</div>
+ */
 @Pipe({
   name: 'exponentialBooster',
   standalone: true
 })
 export class ExponentialBoosterPipe implements PipeTransform {
-
-  transform(value: number, exponent:number = 1): unknown {
+  transform(value: number, exponent:number = 1): number {
     return Math.pow(value, exponent)
   }
 
