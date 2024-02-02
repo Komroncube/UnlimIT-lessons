d5d9710b427e7f9256fc31d03b2576770680d285
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 17:46:44 +0500

    Add best practices component to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { PlaygroundComponent } from './pages/playground/playground.component';
+import { BestPracticesComponent } from './components/best-practices/best-practices.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'playground',
         component: PlaygroundComponent
+    },
+    {
+        path: '',
+        component: BestPracticesComponent
     }
 ];
