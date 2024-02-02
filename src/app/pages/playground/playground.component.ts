4a66e4379e9ba61c599bb1e4b142e137008bd3d8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 11:51:34 +0500

    Add PlaygroundComponent with date formatting functionality

---
+++ src/app/pages/playground/playground.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ExponentialBoosterPipe } from "../../pipes/exponential-booster.pipe";
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-playground',
+    standalone: true,
+    templateUrl: './playground.component.html',
+    styleUrl: './playground.component.scss',
+    imports: [ExponentialBoosterPipe, CommonModule]
+})
+export class PlaygroundComponent {
+    toggle = true;
+    /**
+     * @description This method is used to toggle the format of the date
+     */
+    toggleFormat() {
+        this.toggle = !this.toggle;
+    }
+    /**
+     * @description This property is used to get the format of the date
+     */
+    get format() { return this.toggle ? 'mediumDate' : 'fullDate'; }
+    birthday = new Date(2003, 7, 7);
+
+    today = new Date();
+}
