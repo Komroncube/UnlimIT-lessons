b74c18438950c257d2fccfc446436ac7e1b53013
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 14:07:56 +0500

    Add FlyingHeroesPipe to filter heroes that can fly

---
+++ src/app/pipes/flying-heroes.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../components/objects-with-pipes/hero';
+
+@Pipe({
+  name: 'flyingHeroes',
+  standalone: true
+})
+export class FlyingHeroesPipe implements PipeTransform {
+  transform(allHeroes:Hero[]): Hero[] {
+    return allHeroes.filter(hero => hero.canFly);
+  }
+}
