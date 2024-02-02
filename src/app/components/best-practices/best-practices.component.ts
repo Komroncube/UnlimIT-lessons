f6c88f735dae8f1ada5f59b18726b1bced5ea363
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 18:15:25 +0500

    Add OnlyLetterDirective and MyCurrencyPipe to BestPracticesComponent

--- src/app/components/best-practices/best-practices.component.ts
+++ src/app/components/best-practices/best-practices.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { OnlyLetterDirective } from '../../directives/only-letter.directive';
+import { CommonModule } from '@angular/common';
+import { MyCurrencyPipe } from "../../pipes/my-currency.pipe";
 
 @Component({
-  selector: 'app-best-practices',
-  standalone: true,
-  imports: [OnlyLetterDirective],
-  templateUrl: './best-practices.component.html',
-  styleUrl: './best-practices.component.scss'
+    selector: 'app-best-practices',
+    standalone: true,
+    templateUrl: './best-practices.component.html',
+    styleUrl: './best-practices.component.scss',
+    imports: [
+        OnlyLetterDirective,
+        CommonModule,
+        MyCurrencyPipe
+    ]
 })
 export class BestPracticesComponent {
 
