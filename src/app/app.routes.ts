a50db6924c7d0f54fc9695f19af6576c45d95b07
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 11:50:44 +0500

    Add PlaygroundComponent to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PlaygroundComponent } from './pages/playground/playground.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PlaygroundComponent
+    }
+];
