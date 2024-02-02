149eb7f50a45e39a897cf6134512b794f14e12fe
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 19:25:31 +0500

    Add CommonModule import to directives-working.component.ts

--- src/app/components/directives-working/directives-working.component.ts
+++ src/app/components/directives-working/directives-working.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directives-working',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './directives-working.component.html',
   styleUrl: './directives-working.component.scss'
 })
