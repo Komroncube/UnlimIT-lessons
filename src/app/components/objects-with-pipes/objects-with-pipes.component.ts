4d7956a63e771006fce2b5105f740e9031fc8a50
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 14:07:47 +0500

    Add objects-with-pipes component and update template

---
+++ src/app/components/objects-with-pipes/objects-with-pipes.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from './hero';
+import { FlyingHeroesPipe } from "../../pipes/flying-heroes.pipe";
+
+@Component({
+    selector: 'app-objects-with-pipes',
+    standalone: true,
+    templateUrl: './objects-with-pipes.component.html',
+    styleUrl: './objects-with-pipes.component.scss',
+    imports: [FormsModule, CommonModule, FlyingHeroesPipe]
+})
+export class ObjectsWithPipesComponent {
+  value:string="";
+
+  heroes:Hero[]=[]
+  canFly = true;
+  
+  addHero()
+  {
+    // this.value = this.value.trim()
+    console.log(this.value);
+    if(!this.value) return;
+    const hero = { name:this.value, canFly:this.canFly }
+    this.heroes.push(hero);
+  }
+  reset() {this.heroes = this.heroes.slice(0,0)}
+}
