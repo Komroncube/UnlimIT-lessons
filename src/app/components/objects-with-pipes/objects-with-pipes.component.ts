37282e5e612689f15bd531068518f10aad7278a3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-02 14:34:34 +0500

    Fix flyingHeroesImpure pipe usage in objects-with-pipes component

--- src/app/components/objects-with-pipes/objects-with-pipes.component.ts
+++ src/app/components/objects-with-pipes/objects-with-pipes.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from './hero';
 import { FlyingHeroesPipe } from "../../pipes/flying-heroes.pipe";
+import { FlyingHeroesImpurePipe } from '../../pipes/flying-heroes-impure.pipe';
+
 
 @Component({
     selector: 'app-objects-with-pipes',
     standalone: true,
     templateUrl: './objects-with-pipes.component.html',
     styleUrl: './objects-with-pipes.component.scss',
-    imports: [FormsModule, CommonModule, FlyingHeroesPipe]
+    imports: [FormsModule, CommonModule, FlyingHeroesPipe, FlyingHeroesImpurePipe]
 })
 export class ObjectsWithPipesComponent {
   value:string="";
@@ -24,6 +26,7 @@ export class ObjectsWithPipesComponent {
     if(!this.value) return;
     const hero = { name:this.value, canFly:this.canFly }
     this.heroes.push(hero);
+    this.value = "";
   }
   reset() {this.heroes = this.heroes.slice(0,0)}
 }
