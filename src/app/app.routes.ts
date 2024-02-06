38de3eddf0b28f4f4b4e04f0c707309d05244c6f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-06 20:36:00 +0500

    Add NgContentComponent to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PlaygroundComponent } from './pages/playground/playground.component';
 import { BestPracticesComponent } from './components/best-practices/best-practices.component';
 import { NgTemplateComponent } from './components/ng-template/ng-template.component';
+import { NgContentComponent } from './components/ng-content/ng-content.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +18,9 @@ export const routes: Routes = [
     {
         path: 'ng-template',
         component: NgTemplateComponent
+    },
+    {
+        path: 'ng-content',
+        component: NgContentComponent
     }
 ];
