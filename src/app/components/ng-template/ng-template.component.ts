823e5e328a56940807375c76600235f2c672fb07
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-06 20:53:11 +0500

    Update ng-template component HTML and import NgContentComponent

--- src/app/components/ng-template/ng-template.component.ts
+++ src/app/components/ng-template/ng-template.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgContentComponent } from "../ng-content/ng-content.component";
 
 @Component({
-  selector: 'app-ng-template',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './ng-template.component.html',
-  styleUrl: './ng-template.component.scss'
+    selector: 'app-ng-template',
+    standalone: true,
+    templateUrl: './ng-template.component.html',
+    styleUrl: './ng-template.component.scss',
+    imports: [CommonModule, NgContentComponent]
 })
 export class NgTemplateComponent {
 
