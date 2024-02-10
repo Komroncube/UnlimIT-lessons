b066b4cc40ddf088b66002521fd54b6300d170e0
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-10 12:21:08 +0500

    Refactor code and add enum for roles

--- typescript/types.js
+++ typescript/types.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var console_1 = require("console");
 var person = {
     firstName: "John",
     lastName: "Doe",
@@ -9,4 +6,15 @@ var person = {
 Object.keys(person).forEach(function (key) {
     console.log(key + ': ' + person[key]);
 });
-(0, console_1.log)('Hello World');
+console.log('Hello World');
+//ENUMS
+var Role;
+(function (Role) {
+    Role[Role["User"] = 5] = "User";
+    Role[Role["Moderator"] = 6] = "Moderator";
+    Role["Admin"] = "4";
+})(Role || (Role = {}));
+var admin = Role.Admin;
+console.log(admin); // 4
+console.log(Role[5]);
+console.log(Role['4']);
--- typescript/types.ts
+++ typescript/types.ts
@@ -1,5 +1,3 @@
-import { log } from "console";
-
 let person = {
     firstName : "John",
     lastName : "Doe",
@@ -12,4 +10,18 @@ type PersonKeys = keyof Person; // "firstName" | "lastName" | "age"
 Object.keys(person).forEach((key) => {
     console.log(key + ': ' +  person[key as PersonKeys]);
 });
-log('Hello World');
+console.log('Hello World');
+
+//ENUMS
+enum Role {
+    User = 5,
+    Moderator,
+    Admin = '4',
+}
+
+let admin = Role.Admin;
+console.log(admin); // 4
+console.log(Role[5]);
+console.log(Role['4']);
+
+type RoleKeys = keyof typeof Role;
